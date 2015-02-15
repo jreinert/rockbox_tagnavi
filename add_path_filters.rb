e8c10f3ac6587c631f2241c1ea91ae6e84411a6d
Joakim Reinert <mail@jreinert.com>
2015-02-15 13:56:50 +0100

    Support multiple possible paths in pathfilter

--- add_path_filters.rb
+++ add_path_filters.rb
@@ -1,19 +1,31 @@
-active_filter = nil
+regex = /(?<head>->.*?)\s*(?<mid>\?\s+(?<condition>.*?(?=->|$))|->|$)(?<tail>.*$)/
+active_filters = []
 $stdin.each do |line|
   if line =~ /^\s*#\s*PATHFILTER:\s*"(.*?)"/
-    active_filter = $1
+    active_filters = $1.split('|')
   end
-  puts line.chomp.sub(/(->.*?)\s*(\?|->|$)/) {|match|
-    modified_line = match
-    head = $1
-    tail = $2
-    if line !~ /filename \^ "/
-      modified_line = %Q(#{head} ? filename ^ "#{active_filter}")
-      case(tail)
-      when '?' then modified_line << ' &'
-      when '->' then modified_line << ' ->'
-      else ''
+  puts line.chomp.sub(regex) {|string|
+    match = Regexp.last_match
+    modified_line = string
+    mid = match[:mid]
+    condition = match[:condition]
+    if line !~ /# PATHFILTER APPLIED$/
+      mid_replacement = "? "
+      if condition
+        ors = condition.split(/\s+\|\s+/).map { |part| part.split(/\s+&\s+/) }
+        mid_replacement << active_filters.map { |filter|
+          ors.map { |ands|
+            [%Q(filename ^ "#{filter}"), *ands].join(' & ')
+          }.join(' | ')
+        }.join(' | ')
+      else
+        mid_replacement << active_filters.map { |filter|
+          %Q(filename ^ "#{filter}")
+        }.join(' | ')
       end
+
+      modified_line.sub!(mid, mid_replacement)
+      modified_line << " # PATHFILTER APPLIED"
     end
     modified_line
   }
