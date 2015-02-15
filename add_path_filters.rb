bc75ead17b8e4fa8fe0d06e26389aa881321bc39
Joakim Reinert <mail@jreinert.com>
2015-02-15 16:23:25 +0100

    Fixed bug with lines containing no initial condition

--- add_path_filters.rb
+++ add_path_filters.rb
@@ -19,24 +19,29 @@ io.each do |line|
   puts line.chomp.sub(regex) {|string|
     match = Regexp.last_match
     modified_line = string
+    head = match[:head]
     mid = match[:mid]
     condition = match[:condition]
     if line !~ /# PATHFILTER APPLIED$/
-      mid_replacement = "? "
+      replacement = "? "
       if condition
         ors = condition.split(/\s+\|\s+/).map { |part| part.split(/\s+&\s+/) }
-        mid_replacement << active_filters.map { |filter|
+        replacement << active_filters.map { |filter|
           ors.map { |ands|
             [%Q(filename ^ "#{filter}"), *ands].join(' & ')
           }.join(' | ')
         }.join(' | ')
       else
-        mid_replacement << active_filters.map { |filter|
+        replacement << active_filters.map { |filter|
           %Q(filename ^ "#{filter}")
         }.join(' | ')
       end
 
-      modified_line.sub!(mid, mid_replacement)
+      if condition
+        modified_line.sub!(mid, replacement) unless condition =~ /[~\^]\s+""/
+      else
+        modified_line.sub!(head, head + " #{replacement}")
+      end
       modified_line << " # PATHFILTER APPLIED"
     end
     modified_line
